b344026325792a39cfb3770ad2ed287e3e8efe55
Yousof Mardoukhi <ymardoukhi@gmail.com>
2022-08-10 16:07:24 +0200

    changing the class structure

--- src/BoseSampSimulation.py
+++ src/BoseSampSimulation.py
@@ -5,26 +5,28 @@ class BoseSampSim:
     def __init__(self, programme, engine) -> None:
         self.programme = programme
         self.engine = engine
+        self.two_shots = None
+        self.von_neumann_str = None
 
 
     def two_shot_simulation(self):
         res = []
         for i in range(2):
             output = self.engine.run(self.programme).samples[0]
-            res.append(self.convert_binary(output))
+            res.append(self._convert_binary(output))
         
-        return res
+        self.two_shots = res
 
-    def convert_binary(self, array):
+    def _convert_binary(self, array):
         array[np.where(array > 0)[0]] = 1
         return array
 
-    def von_neumann_prot(input1, input2):
+    def von_neumann_prot(self):
         output_res = ''
-        for i in range(len(input1)):
-            if input1[i] == input2[i]:
+        for i in range(len(self.two_shots[0])):
+            if self.two_shots[0][i] == self.two_shots[1][i]:
                 continue
             else:
-                digit = abs(input1[i]*(input2[i]-1))
+                digit = abs(self.two_shots[0][i]*(self.two_shots[1][i]-1))
                 output_res = output_res + str(digit)
-        return output_res
+        self.von_neumann_str = output_res
