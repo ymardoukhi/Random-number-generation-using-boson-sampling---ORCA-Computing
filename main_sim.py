6adac356a9bb9298d2ea2c1ae47f3ce42b410e39
Yousof Mardoukhi <ymardoukhi@gmail.com>
2022-08-14 16:18:31 +0200

    added the possibility to parse arguments instead of changing the source code itself

--- main_sim.py
+++ main_sim.py
@@ -24,13 +24,23 @@ def naive_uniform_sim(n, m, d, num_params, v, sim_bool, seed):
     return bose_sim.von_neumann_str
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n', type=int, help='number of photos')
+    parser.add_argument('m', type=int, help='number of modes')
+    parser.add_argument('d', type=int, help='depth of the circuit')
+    parser.add_argument('n_param', type=int, help='number of beam-spliters')
+    parser.add_argument('v', type=int, help='version of the architecture')
+    args = parser.parse_args()
 
     N = 10**4
     np.random.seed(42)
     seeds = np.random.randint(N, size=N)
 
     output_strs = jb.Parallel(n_jobs=10, verbose=5)(
-        jb.delayed(bose_sampling_sim)(seed) for seed in seeds)
+        jb.delayed(naive_uniform_sim)(
+            args.n, args.m, args.d, args.n_param,
+            args.v, True, seed
+            ) for seed in seeds)
 
     output_strs = list(filter(lambda i: i != '', output_strs))
     
