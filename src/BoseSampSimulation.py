616513433b1a29e50a01366cfed8a4c3c2867815
Yousof Mardoukhi <ymardoukhi@gmail.com>
2022-08-14 20:35:22 +0200

    code chunck responsible for producing random string using the Huffman encoding

--- src/BoseSampSimulation.py
+++ src/BoseSampSimulation.py
@@ -1,10 +1,13 @@
 import numpy as np
+import joblib as jb
 
 class BoseSampSim:
 
-    def __init__(self, programme, engine) -> None:
+    def __init__(self, programme, engine, params_dict, encoding=None) -> None:
         self.programme = programme
         self.engine = engine
+        self.args = params_dict
+        self.encoding = encoding
         self.two_shots = None
         self.von_neumann_str = None
 
@@ -16,6 +19,21 @@ class BoseSampSim:
             res.append(self._convert_binary(output))
         
         self.two_shots = res
+
+    def _hufmann_str(self):
+        output = self.engine.run(self.programme, args=self.args).samples[0]
+        return self.encoding[str(tuple(output))]
+
+    def hufmann_simulation(self, shots):
+        two_str = []
+        for _ in range(2):
+            str_ls = []
+
+            for _ in range(shots):
+                str_ls.append(self._hufmann_str())
+
+            two_str.append("".join(str_ls))
+        self.two_shots = two_str
 
     def _convert_binary(self, array):
         array[np.where(array > 0)[0]] = 1
