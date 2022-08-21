fcde58895cbb0f0df258bc73355d460ac791fa41
Yousof Mardoukhi <ymardoukhi@gmail.com>
2022-08-21 11:37:52 +0200

    statistical analyses are moved to PostAnalysis source

--- borealis_local.py
+++ borealis_local.py
@@ -23,30 +23,10 @@ def borealis_sampler(prog: sf.program.Program) -> np.ndarray:
     eng = sf.Engine(backend="gaussian")
     return eng.run(prog, crop=True).samples[0]
 
-def one_zero_ratio(binary_string: str) -> dict:
-    """
-    a function that calculates the ration between zeros 
-    and ones in a binary string
-
-    input:
-        binary_string (str): a given binary string e.g. 010001
-    
-    return:
-        _ (dict): dictionary of the form {'0': 1-ratio, '1': ratio} 
-            where ration is the ration between zeros and ones
-    """
-    # convert the binary strings into an array of zero and 
-    # one integers
-    binary_arr = np.asarray([*binary_string], dtype=int)
-    # sum is the total number of ones
-    ratio = np.sum(binary_arr)/binary_arr.shape[0]
-    return {'0': 1-ratio, '1': ratio}
-
-
 def main():
 
     modes = 4
-    shots = int(1e2)
+    shots = int(1e4)
     output_path = "./data/borealis"
 
     # squeezing-gate parameters
@@ -99,23 +79,6 @@ def main():
     # same the output_strs for post analysis
     with open("{}/output_strs_local_N{}.json".format(output_path, shots), "w") as f:
         json.dump(output_strs, f)
-    # calculate the ratio of 1's and 0's in the concatenation of
-    # all those binary strings
-    ratio = one_zero_ratio("".join(output_strs))
-    # calculate the statistics of the binary strings and sort 
-    # the probability of their appearance in a descending manner
-    output_strs = Counter(output_strs)
-    output_strs = [(key, val) for key, val in output_strs.items()]
-    output_strs.sort(key=lambda x: x[1], reverse=True)
-    output_strs = {el[0]: el[1]/shots/2 for el in output_strs}
-    
-    # save the statistics of 01 ratio and the binary strings
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-    with open("{}/local_N{}.json".format(output_path, shots), "w") as f:
-        json.dump(output_strs, f)
-    with open("{}/ratio_local_N{}.json".format(output_path, shots), "w") as f:
-        json.dump(ratio, f)
 
 if __name__ == "__main__":
     main()
